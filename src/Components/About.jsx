07e2bba94489233499c70d7592178e0355c05cc2
Chikkala Pranay <pranaychikkala0@gmail.com>
2025-08-15 15:50:34 +0000

    "Updated About section"

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,14 +1,13 @@
 function About() {
-    return (
-      <section style={{ padding: "2rem" }}>
-        <h2>About Me</h2>
-        <p>
-          Hi! I’m a passionate learner exploring React and web development.  
-          This is my first portfolio website.
-        </p>
-      </section>
-    );
-  }
-  
-  export default About;
-  
+  return (
+    <section style={{ padding: "2rem" }}>
+      <h2>About Me</h2>
+      <p>
+        Hi! I'm a passionate learner in Human Resources and technology. This is
+        my first portfolio project.
+      </p>
+    </section>
+  );
+}
+
+export default About;
